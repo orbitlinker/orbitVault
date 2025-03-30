81f7e2c681ccb053f8dd11b1abb4d0d5b471292c
orbitlinker <orbitlinker01@gamil.com>
2025-03-30 20:08:30 +0530

    side bar done

--- src/modules/sideBar/Sidebar.tsx
+++ src/modules/sideBar/Sidebar.tsx
@@ -1,12 +1,21 @@
-import { SidebarComponent, SelectorTab, SidebarWrapper } from "./style"
-
+import { SidebarComponent, ListContainer, SelectorTab, SidebarWrapper } from "./style"
+import { IoLink } from "react-icons/io5";
+import { MdAttachEmail } from "react-icons/md";
 const Sidebar = () => {
     return (
         <SidebarComponent>
             <SidebarWrapper>
                 <SelectorTab>
-                    <p>Dashboad</p>
+                    <div className="headerContainer">
+                        <h1>Orbit <IoLink style={{ transform: "rotate(-45deg)" }} /> Linker</h1>
+                    </div>
                 </SelectorTab>
+                <ListContainer>
+                    <a className="listHandler listHandlerActive" href="/">
+                        <MdAttachEmail />
+                        <li>send Email</li>
+                    </a>
+                </ListContainer>
             </SidebarWrapper>
         </SidebarComponent>
     )
--- src/modules/sideBar/style.ts
+++ src/modules/sideBar/style.ts
@@ -1,26 +1,74 @@
 import styled from "styled-components";
 
-
 export const SidebarComponent = styled.section`
-    background: #000;
-    max-width:400px;
+    background: #18283b;
+    max-width:256px;
     width: 40%;
     position: fixed;
     top: 0;
     bottom: 0;
     left: 0;
-    box-shadow: 1px 1px 20px #00000029;
-    p{
-        color: #fff;
+    box-shadow: 1px 10px 10px rgb(209 213 219);
+    h1{
+        color: rgb(209 213 219);
+        font-size: 18px;
+        font-weight: 600;
+        text-transform: uppercase;
+        display: flex;
+        align-items: center;
+        gap: 4px;
     }
 `
 export const SidebarWrapper = styled.section`
-    padding: 20px;
-    width: 90%;
-    margin: 0 auto;
+    p{
+        font-size: 21px;
+    }
 `
 export const SelectorTab = styled.div`
-    p{
-        color: #fff;
+    padding: 20px 0px;
+    margin: 10px 20px;
+    /* background: #131313; */
+    /* border-radius: 10px; */
+    border-bottom: 1px solid #ffffff24;
+    .headerContainer{
+        display: flex;
+        align-items: center;
+        padding: 0 30px;
+        gap: 1.5rem;
+    }
+`
+export const ImgWrapper = styled.div`
+    width: 40px;
+    img{
+        width: 100%;
+    }
+`
+export const ListContainer = styled.div`
+    margin: 20px 0;
+    svg{
+        font-size: 21px;
+    }
+    li{
+        font-family: "Nunito", sans-serif;
+        text-transform: uppercase;
+        font-size: 14px;
+    }
+    .listHandler{
+        transition: 250ms;
+        display: flex;
+        gap: 1.5rem;
+        color: #8392a5;
+        align-items: center;
+        padding: 16px 20px;
+        border-radius: 10px;
+        margin-left: 20px;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+    }
+    .listHandler:hover{
+        background: #2c3e50;
+    }
+    .listHandlerActive{
+        background:rgb(236 237 238);
     }
 `
